8054ba0e6800a17922d5d6ba999a18e7c3d03bc2
Louis Garavaglia <louis.garavaglia@gmail.com>
2020-09-21 11:25:30 -0700

    fixed broken post route

--- broken-app/app.js
+++ broken-app/app.js
@@ -2,17 +2,35 @@ const express = require('express');
 let axios = require('axios');
 var app = express();
 
-app.post('/', function(req, res, next) {
-  try {
-    let results = req.body.developers.map(async d => {
-      return await axios.get(`https://api.github.com/users/${d}`);
-    });
-    let out = results.map(r => ({ name: r.data.name, bio: r.data.bio }));
+app.use(express.json())
+app.use(express.urlencoded({extended:true}))
 
-    return res.send(JSON.stringify(out));
-  } catch {
-    next(err);
+app.post('/', async function(req, res, next) {
+  try {
+    let devs = req.body.developers
+    const devArray = [];
+    for (let i = 0; i < devs.length; i++) {
+      devArray.push(axios.get(`https://api.github.com/users/${devs[i]}`)) 
+    }
+    async function getDevResults() {
+      let results = await Promise.all(devArray);
+      let out = results.map(r => ({ name: r.data.name, bio: r.data.bio }));
+      return out;
+    }
+    const out = await getDevResults();
+    console.log(out);
+    return res.json(out);
+  } catch(e) {
+    next(e);
   }
+});
+
+app.use(function (err, req, res, next) {
+  res.status(err.status || 500);
+  return res.json({
+    error: err,
+    message: err.message
+  });
 });
 
 app.listen(3000);
--- broken-app/issues.md
+++ broken-app/issues.md
@@ -1,1 +1,7 @@
 # Broken App Issues
+* Missing app.use(express.json()).
+* Missing argument in the catch keyword to pass to the next object.
+* Mapping over req.body.developers to await the api results. This could be faster by implementing Promise.All() so that's what I did.
+* In order to get the results of my function that returned the fulfilled promise from the API, I would need to call that function and await it. In order to do that, I needed to make the post route call back function Asynchronous so I added the async keyword in front of the call back function.
+* Can simply use return res.json(out);, instead of return res.send(JSON.stringify(out));.
+* Missing an app.use(error, req, res, next) error route to handle any errors that may occur.
